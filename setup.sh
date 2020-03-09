d6733cacc46d5dc312fba28a2a34eacd45733d9d
Harry Stebbins <hstebbins@mdsol.com>
2020-03-09 17:46:41 -0400

    Initialization

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# install brew and get cookin
+
+# rvm
+# iterm2
+
+# move dots/bash_profile to ~/.bash_profile
+# move dots/spacemacs to ~/.spacemacs
+
+# touch ~/org/todo.org
+
+# org to latex engine?
+
+# mononoki font
+# spacemacs theme: madhat2r
